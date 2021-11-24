89f3cd26201f1e358b658468b5d08339e97438d5
bian zhou <bianzhou23@gmail.com>
2021-11-24 23:17:43 +1100

    feature-a to title

---
+++ src/db.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD_kpGDJgYGdR_wCNzBtRM8t1J2ttjnm4s",
+    authDomain: "chatroom-6d36e.firebaseapp.com",
+    projectId: "chatroom-6d36e",
+    storageBucket: "chatroom-6d36e.appspot.com",
+    messagingSenderId: "628313599725",
+    appId: "1:628313599725:web:8e65b685c68932a6f666e4",
+    measurementId: "G-88KNTFGZ9C"
+  };
+
+  
